6390077218b6a20256ff81f92ef59b5d360dc6e7
Ernesto Segundo <rsernesto@gmail.com>
2021-11-15 18:00:33 -0500

    Uso de base de datos en Django - Cambio de base de datos del proyecto tienda online, PostgreSQL

--- tiendaonline/tiendaonline/settings.py
+++ tiendaonline/tiendaonline/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tiendaonline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tiendaonline',
+        'USER': 'postgres',
+        'PASSWORD': '3rnest0....*',
+        'HOST': 'localhost',
+        'DATABASE_PORT': '5432',
     }
 }
 
