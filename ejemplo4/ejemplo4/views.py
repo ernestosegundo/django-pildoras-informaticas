2133744bfe2047572e87cdd82e012dd9f8af939b
Ernesto Segundo <rsernesto@gmail.com>
2021-11-15 12:24:28 -0500

    Ejemplo de uso de plantillas en Django - Cargadores de plantillas

---
+++ ejemplo4/ejemplo4/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.template.loader import get_template
+
+def get_index(request):
+    la_plantilla = get_template("index.html")
+
+    el_documento = la_plantilla.render({"saludo": "Hola", "despedida": "Adios"})
+
+    return HttpResponse(el_documento)
