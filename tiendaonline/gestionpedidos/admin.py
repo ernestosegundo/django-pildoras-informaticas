32c0cf968e26fbc469d351bde5bd764376834267
Ernesto Segundo <rsernesto@gmail.com>
2021-11-15 23:42:19 -0500

    Uso de base de datos en Django - Personalización del panel de administración - Cambio de idioma

--- tiendaonline/gestionpedidos/admin.py
+++ tiendaonline/gestionpedidos/admin.py
@@ -5,14 +5,18 @@ from gestionpedidos.models import Cliente, Articulo, Pedido
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ("nombre", "direccion", "telefono")
     search_fields = ("nombre", "telefono")
+    list_filter = ("direccion",)
 
 class ArticuloAdmin(admin.ModelAdmin):
     list_display = ("nombre", "seccion", "precio")
     search_fields = ("nombre", "seccion")
+    list_filter = ("seccion",)
 
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ("numero", "fecha", "entregado")
     search_fields = ("numero", "fecha")
+    list_filter = ("fecha",)
+    date_hierarchy = "fecha"
 
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Articulo, ArticuloAdmin)
