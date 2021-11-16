0903835409e0c542d4a0736add5908baf94ff5dd
Ernesto Segundo <rsernesto@gmail.com>
2021-11-16 10:59:27 -0500

    Formularios en Django - Formulario de búsqueda en la base de datos

--- tiendaonline/gestionpedidos/views.py
+++ tiendaonline/gestionpedidos/views.py
@@ -1,12 +1,23 @@
+from django.db.models import query
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from gestionpedidos.models import Articulo
 
 # Create your views here.
 def buscar_articulos(request):
     return render(request, "buscararticulos.html")
 
 def buscar(request):
-    mensaje = "Artículo buscado: %r" %request.GET["articulo"]
+    if request.GET["articulo"]:
+        mensaje = "Artículo buscado: %r" %request.GET["articulo"]
+
+        articulo = request.GET["articulo"]
+
+        articulos = Articulo.objects.filter(nombre__icontains=articulo)
+
+        return render(request, "resultadosbusquedaarticulos.html", {"articulos": articulos, "query": articulo})
+    else:
+        mensaje = "No se ingresó el artículo a buscar"
 
     return HttpResponse(mensaje)
 
