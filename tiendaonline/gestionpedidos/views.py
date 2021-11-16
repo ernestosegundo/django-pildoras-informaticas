4183d9b0737c3cc4b4283f39a67fff49688b371f
Ernesto Segundo <rsernesto@gmail.com>
2021-11-16 10:11:40 -0500

    Uso de base de datos en Django - Formulario de búsqueda - Configuración inicial

--- tiendaonline/gestionpedidos/views.py
+++ tiendaonline/gestionpedidos/views.py
@@ -1,3 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def buscar_articulos(request):
+    return render(request, "buscararticulos.html")
+
+def buscar(request):
+    mensaje = "Artículo buscado: %r" %request.GET["articulo"]
+
+    return HttpResponse(mensaje)
+
--- tiendaonline/tiendaonline/urls.py
+++ tiendaonline/tiendaonline/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestionpedidos.views import buscar_articulos, buscar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('buscar-articulos/', buscar_articulos),
+    path('buscar/', buscar),
 ]
