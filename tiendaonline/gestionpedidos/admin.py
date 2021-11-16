610ef2042d47b53707e63861cc8490a9695ef257
Ernesto Segundo <rsernesto@gmail.com>
2021-11-15 23:25:08 -0500

    Uso de base de datos en Django - Personalización del panel de administración

--- tiendaonline/gestionpedidos/admin.py
+++ tiendaonline/gestionpedidos/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from gestionpedidos.models import Cliente, Articulo, Pedido
 
 # Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Articulo)
-admin.site.register(Pedido)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "direccion", "telefono")
+    search_fields = ("nombre", "telefono")
+
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "seccion", "precio")
+    search_fields = ("nombre", "seccion")
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ("numero", "fecha", "entregado")
+    search_fields = ("numero", "fecha")
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Articulo, ArticuloAdmin)
+admin.site.register(Pedido, PedidoAdmin)
--- tiendaonline/gestionpedidos/models.py
+++ tiendaonline/gestionpedidos/models.py
@@ -3,19 +3,22 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
-    email = models.EmailField(blank=True, null=True)
-    telefono = models.CharField(max_length=7)
+    direccion = models.CharField(max_length=50, verbose_name="Dirección")
+    email = models.EmailField(blank=True, null=True, verbose_name="Correo Electrónico")
+    telefono = models.CharField(max_length=7, verbose_name="Teléfono")
+
+    def __str__(self):
+        return self.nombre
 
 class Articulo(models.Model):
     nombre = models.CharField(max_length=30)
-    seccion = models.CharField(max_length=20)
+    seccion = models.CharField(max_length=20, verbose_name="Sección")
     precio = models.IntegerField()
 
     def __str__(self):
         return "El nombre es %s, la sección es %s y el precio es %s" % (self.nombre, self.seccion, self.precio)
 
 class Pedido(models.Model):
-    numero = models.IntegerField()
+    numero = models.IntegerField(verbose_name="Número")
     fecha = models.DateField()
     entregado = models.BooleanField()
