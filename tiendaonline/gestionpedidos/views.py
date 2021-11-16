848c05bf9dd949387bb2e856782e72eeba9078d3
Ernesto Segundo <rsernesto@gmail.com>
2021-11-16 15:17:58 -0500

    Formularios en Django - Formulario de contacto - Envío de correo electrónico

--- tiendaonline/gestionpedidos/views.py
+++ tiendaonline/gestionpedidos/views.py
@@ -1,6 +1,9 @@
 from django.db.models import query
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.core.mail import message, send_mail
+from django.conf import settings
+
 from gestionpedidos.models import Articulo
 
 # Create your views here.
@@ -26,6 +29,13 @@ def buscar(request):
 
 def contacto(request):
     if request.method == "POST":
-        return render(request, "trabajando.html")
+        subject = request.POST["asunto"]
+        message = request.POST["mensaje"] + " " + request.POST["email"]
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ["rsernesto@gmail.com"]
+
+        send_mail(subject, message, email_from, recipient_list)
+
+        return render(request, "realizado.html", {"realizado": "El mensaje de correo electrónico se envió correctamente. En breve le contactaremos a " + request.POST["email"]})
 
     return render(request, "contacto.html")
