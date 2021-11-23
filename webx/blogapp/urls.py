6ce8f8b63ff9933db6113236afda9b99caa0e820
Ernesto Segundo <rsernesto@gmail.com>
2021-11-23 08:58:25 -0500

    Proyecto guiado - Funcionalidad a categorías

--- webx/blogapp/urls.py
+++ webx/blogapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name="Blog")
+    path('', views.blog, name="Blog"),
+    path('categoria/<int:categoria_id>/', views.categoria, name="Categoria"),
 ]
 
