aaea921ef60d15603cd2c855f39287349ed6a5fb
Ernesto Segundo <rsernesto@gmail.com>
2021-11-24 14:02:25 -0500

    Proyecto guiado - App contacto - Envío de datos por correo electrónico

--- webx/contactoapp/views.py
+++ webx/contactoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from django.core.mail import EmailMessage
+
 from .forms import FormularioContacto
 
 # Create your views here.
@@ -13,6 +15,15 @@ def contacto(request):
             email = request.POST.get("email")
             contenido = request.POST.get("contenido")
 
-            return redirect("/contacto/?valido")
+            email = EmailMessage("Mensaje desde app Django", 
+                    "El usuario con nombre {} con el correo {} escribe lo siguiente:\n\n {}".format(nombre, email, contenido),
+                    "", ["rsernesto@outloook.com"], reply_to=[email])
 
+            try:
+                email.send()
+
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
+                
     return render(request, "contacto/contacto.html", {"formulario_contacto": formulario_contacto})
