62c770410f99ad86bbf325281f53a06995f6171e
Ernesto Segundo <rsernesto@gmail.com>
2021-11-24 10:36:39 -0500

    Proyecto guiado - Formato al formulario de contacto

--- webx/contactoapp/forms.py
+++ webx/contactoapp/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre = forms.CharField(label="Nombre", required=True)
     email = forms.EmailField(label="Correo Electrónico", required=True)
-    contenido = forms.CharField(label="Contenido")
+    contenido = forms.CharField(label="Contenido", widget=forms.Textarea, required=False)
