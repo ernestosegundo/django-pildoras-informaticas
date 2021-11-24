d21140616cc73612068ab5dbdf1049e66a774676
Ernesto Segundo <rsernesto@gmail.com>
2021-11-24 11:15:26 -0500

    Proyecto guiado - App contacto - Envío de datos

--- webx/contactoapp/views.py
+++ webx/contactoapp/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 from .forms import FormularioContacto
 
 # Create your views here.
 def contacto(request):
     formulario_contacto = FormularioContacto()
+
+    if request.method == "POST":
+        formulario_contacto = FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+
+            return redirect("/contacto/?valido")
+
     return render(request, "contacto/contacto.html", {"formulario_contacto": formulario_contacto})
