b4569d5bf277da43dcafa343e924c5f22e24f36d
Ernesto Segundo <rsernesto@gmail.com>
2021-11-25 21:15:37 -0500

    Proyecto guiado - App tienda - Registro en panel de administración

--- webx/tiendaapp/admin.py
+++ webx/tiendaapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.db import models
+from .models import TipoProducto, Producto
 
 # Register your models here.
+class TipoProductoAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+admin.site.register(TipoProducto, TipoProductoAdmin)
+admin.site.register(Producto, ProductoAdmin)
