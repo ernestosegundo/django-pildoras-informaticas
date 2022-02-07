10abd47a944c2a35cc03ffd717d8520dcbea27a5
Ernesto Segundo <rsernesto@gmail.com>
2022-02-07 16:23:05 -0500

    App login: creación de formulario de registro de usuario - Vídeo 60

--- webx/login/urls.py
+++ webx/login/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import Registro
 
 urlpatterns = [
-    path('', views.login, name="Login"),
+    path('', Registro.as_view(), name="Login"),
 ]
--- webx/login/views.py
+++ webx/login/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from django.views.generic import View
+
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
-def login(request):
-    return render(request, "registro/registro.html")
+class Registro(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "registro/registro.html", {"form": form})
+
+    def post(self, request):
+        pass
