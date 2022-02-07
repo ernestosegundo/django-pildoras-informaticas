609bbf3e4a8988a580f7bb0e601b3e27237254ac
Ernesto Segundo <rsernesto@gmail.com>
2022-02-07 12:35:18 -0500

    App login - Vídeo 79

--- taip/taip/urls.py
+++ taip/taip/urls.py
@@ -1,4 +1,4 @@
-"""webx URL Configuration
+"""taip URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,15 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('webxapp.urls')),
-    path('servicios/', include('serviciosapp.urls')),
-    path('blog/', include('blogapp.urls')),
-    path('contacto/', include('contactoapp.urls')),
-    path('tienda/', include('tiendaapp.urls')),
-    path('carro/', include('carroapp.urls')),
-    path('login/', include('login.urls')),
 ]
--- webx/login/apps.py
+++ webx/login/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-class TiendaappConfig(AppConfig):
+
+class LoginConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tiendaapp'
+    name = 'login'
